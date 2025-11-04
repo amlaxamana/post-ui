d43a619a84e259e850f5e1bd3303537aeb266043
Aaron <amlaxamana.student@ua.edu.ph>
2025-11-04 14:34:17 +0800

    Fix Link

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://post-ui-p003.onrender.com',
         changeOrigin: true,
         secure: false
       }
